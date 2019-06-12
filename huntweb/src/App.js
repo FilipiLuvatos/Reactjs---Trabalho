25b991c4c6d21ebbb8e999f3fa72bff5053e7cd7
FilipiLuvatos <37461362+FilipiLuvatos@users.noreply.github.com>
2019-06-12 12:30:58 -0300

    Buscando produtos da API

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,10 +1,13 @@
 import React, {Componet} from 'react';
 import Header from './components/Header'
-
+import Main from './pages/main';
+import api from './services/api';
 
 const App = ()=>(
   <div className = "App">
     <Header/>
+    <Main />
   </div>
 );
 export default App;
+ 

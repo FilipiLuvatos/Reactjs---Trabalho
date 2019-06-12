75413b743c00460e8dcf1f761508a3f639f26321
FilipiLuvatos <37461362+FilipiLuvatos@users.noreply.github.com>
2019-06-12 11:16:36 -0300

    Criando Header

---
+++ huntweb/src/App.js
@@ -0,0 +1,10 @@
+import React, {Componet} from 'react';
+import Header from './components/Header'
+
+
+const App = ()=>(
+  <div className = "App">
+    <Header/>
+  </div>
+);
+export default App;
---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./styles.css"
+
+const Header = () =>  <header id = 'main'>JSHunt</header>
+
+
+
+export default Header;
